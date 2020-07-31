c34e9488f6eed0cb1c606d89e1baf2199e746021
Lucas Garcia <lucas.garcia_021@hotmail.com>
2020-07-30 23:40:32 -0300

    Vue JS <3

---
+++ empreguets/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 style="color: #ff00c8">EMPREGUETS PORRA</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
